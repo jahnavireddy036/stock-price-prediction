7c8f32c503eb5a07d1f0e96db3179686829b034f
jahnavireddy036 <111407739+jahnavireddy036@users.noreply.github.com>
2024-12-06 04:01:53 +0530

    added LSTMModel class

--- model.py
+++ model.py
@@ -1,14 +1,45 @@
-# model.py
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
 
-def create_lstm_model(sequence_length=80):
-    model = Sequential()
-    model.add(LSTM(units=100, return_sequences=True, input_shape=(sequence_length, 1)))
-    model.add(Dropout(0.2))
-    model.add(LSTM(units=100, return_sequences=False))
-    model.add(Dropout(0.2))
-    model.add(Dense(units=1))
+class LSTMModel:
+    def __init__(self, sequence_length=80, units=100, dropout_rate=0.2, optimizer='nadam', loss='mean_squared_error'):
+        """
+        Initializes the LSTMModel class with given parameters.
 
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    return model
+        Parameters:
+            sequence_length (int): The length of the input sequence.
+            units (int): Number of units in the LSTM layers.
+            dropout_rate (float): Dropout rate to prevent overfitting.
+            optimizer (str): Optimizer used during model compilation.
+            loss (str): Loss function used during model compilation.
+        """
+        self.sequence_length = sequence_length
+        self.units = units
+        self.dropout_rate = dropout_rate
+        self.optimizer = optimizer
+        self.loss = loss
+        self.model = self._create_model()
+
+    def _create_model(self):
+        """Creates and compiles the LSTM model."""
+        model = Sequential()
+
+        # First LSTM Layer
+        model.add(LSTM(units=self.units, return_sequences=True, input_shape=(self.sequence_length, 1)))
+        model.add(Dropout(self.dropout_rate))
+
+        # Second LSTM Layer
+        model.add(LSTM(units=self.units, return_sequences=False))
+        model.add(Dropout(self.dropout_rate))
+
+        # Dense Layer
+        model.add(Dense(units=1))
+
+        # Compile the model
+        model.compile(optimizer=self.optimizer, loss=self.loss)
+
+        return model
+
+    def get_model(self):
+        """Returns the compiled LSTM model."""
+        return self.model
